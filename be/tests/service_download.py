cae8d71b52f36b4f85c51b386bc6a641d959b026
standlx <lantianye@163.com>
2025-07-26 23:47:13 +0800

    feat(api): add OpenAPI spec generation script and update file metadata response
    refactor(logging): improve fastapi logger documentation and structure
    fix(redis): correct ping response check in redis manager
    test: update test files with hashed user_id and new file_id
    docs: add deployment todo in README.md

--- be/tests/service_download.py
+++ be/tests/service_download.py
@@ -1,14 +1,19 @@
+import hashlib
 import requests
 
 user_id = "userid_123"
-file_id = "fa43b6889c67ccb96dfcc6d06ad01602b4631a87521d3fd5fbc0bc4195d84372"
-url = f"http://127.0.0.1:8000/download/{user_id}/{file_id}"
+user_id = hashlib.sha256(user_id.encode()).hexdigest()
+file_id = "7f8458b670fb5467157a1696457d48e7041d27cc9209278c060c5b5b37fa55bb"
+download_url = f"http://127.0.0.1:8000/download/{user_id}/{file_id}"
+filename_url = f"http://127.0.0.1:8000/files/{user_id}/{file_id}"
 
-response = requests.get(url, timeout=10)
+response = requests.get(filename_url, timeout=10)
+filename = response.json()["filename"]
 
+response = requests.get(download_url, timeout=10)
 if response.status_code == 200:
     print("文件下载成功！")
-    with open(f"{file_id}.pdf", "wb") as f:
+    with open(f"{filename}", "wb") as f:
         f.write(response.content)
 else:
     print(f"文件下载失败，状态码: {response.status_code}")
--- be/tests/service_get_file_status.py
+++ be/tests/service_get_file_status.py
@@ -1,6 +1,6 @@
 import requests
 
-FILE_ID = "a0b111ca3407ff23e269215869205257c1a0235d8376ff4678784bf367d8ae0b"
+FILE_ID = "7f8458b670fb5467157a1696457d48e7041d27cc9209278c060c5b5b37fa55bb"
 url_file_id = f"http://127.0.0.1:8000/file_status/{FILE_ID}"
 
 response = requests.get(url_file_id, timeout=10)
